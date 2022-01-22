5066f288edd62f76a321a89c2e30135eba533518
Jeff Hokit <jhokit@rocketcom.com>
2022-01-22 11:48:02 -0800

    chore: Document and code cleanup

--- Shared/ColorSchemeAutomatic.swift
+++ Shared/ColorSchemeAutomatic.swift
@@ -8,16 +8,36 @@
 import Foundation
 import SwiftUI
 
+// A variety of utilities to switch a View among automatic (system), light, and dark color schemes.
+//
+// The problem:
+// The system enum ColorScheme only has light and dark values.
+// The system ViewModifier preferredColorScheme only allows light and dark values.
+// To use the system's current color scheme, a view must be unmodified. There is no value that can be passed to preferredColorScheme to choose the system scheme
+//
+// The solutions:
+// Provide an enum, ViewModifier, ToolbarContent, and an AppStorage value that support switching among automatic (system), light, and dark schemes.
+//
+// Note that many solutions try to use "@Environment(\.colorScheme) var colorScheme" to detect the system theme, but this actually returns the current view's scheme.
+// Once the view's scheme has been manually set to light or dark, (\.colorScheme) will return light or dark, regardless of the system's scheme.
+// The best solution is to leave a view's color scheme unmodified to use the system theme.
 
+// An enum that adds automatic mode, missing from ColorScheme
 enum ColorSchemeAutomatic:Int{
     case automatic
     case light
     case dark
 }
 
+// The name of the AppStorage item that holds and stores the selected ColorSchemeAutomatic value.
+// Usage:
+//      @AppStorage(colorSchemeAutomaticName) var colorSchemeAutomatic:ColorSchemeAutomatic = .automatic
 let colorSchemeAutomaticName = "ColorSchemeAutomatic"
 
 
+// A ViewModifier that applies the current value to a view
+// Usage:
+//      .modifier(automaticColorScheme())
 struct automaticColorScheme: ViewModifier {
     @AppStorage(colorSchemeAutomaticName) var colorSchemeAutomatic:ColorSchemeAutomatic = .automatic
 
@@ -28,50 +48,41 @@ struct automaticColorScheme: ViewModifier {
         else if (colorSchemeAutomatic == .dark) {
             content.preferredColorScheme(.dark)
         }
-        else {
-            content
+        else { // automatic
+            content // return the unmodified view content
         }
     }
 }
 
-
+// ToolbarContent that adds a ToolbarItem and emits a menu to switch color schemes
+//               .toolbar {
+//                   AutomaticColorToolbarContent()
+//                }
 struct AutomaticColorToolbarContent: ToolbarContent  {
     var placement:ToolbarItemPlacement = .automatic
     @AppStorage(colorSchemeAutomaticName) var colorSchemeAutomatic:ColorSchemeAutomatic = .automatic
 
     var body: some ToolbarContent {
-        ToolbarItem(placement: .navigationBarTrailing)  {
+        ToolbarItem(placement: placement)  {
 
             Menu(content: {
                 
                 Button(action : {
                     colorSchemeAutomatic = .automatic
                 }) {
-                    if colorSchemeAutomatic == .automatic {
-                       Label("Automatic", systemImage: "checkmark")
-                    } else {
-                       Text("Automatic")
-                    }
+                    Label("Automatic", systemImage: colorSchemeAutomatic == .automatic ? "checkmark" : "")
                 }
                 
                 Button(action : {
                     colorSchemeAutomatic = .light
                 }) {
-                    if colorSchemeAutomatic == .light {
-                       Label("Light", systemImage: "checkmark")
-                    } else {
-                       Text("Light")
-                    }
+                    Label("Light", systemImage: colorSchemeAutomatic == .light ? "checkmark" : "")
                 }
 
                 Button(action : {
                     colorSchemeAutomatic = .dark
                 }) {
-                    if colorSchemeAutomatic == .dark {
-                       Label("Dark", systemImage: "checkmark")
-                    } else {
-                       Text("Dark")
-                    }
+                    Label("Dark", systemImage: colorSchemeAutomatic == .dark ? "checkmark" : "")
                 }
             })
             {
