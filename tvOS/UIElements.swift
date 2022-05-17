f31fdcdbabaaad14273e97d7788d7ff3a09738b0
Jeff Hokit <jhokit@rocketcom.com>
2022-05-17 13:00:51 -0700

    improved tvOS UI

--- tvOS/UIElements.swift
+++ tvOS/UIElements.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AstroSwiftFoundation
 
 struct UIElements: View {
     @State var name: String = ""
@@ -17,43 +18,62 @@ struct UIElements: View {
     @State private var isShowingSpinner = false
     @State private var isShowingAlert = false
     @ObservedObject private var fruits = Fruits()
-
+    @State private var status:AstroStatus = AstroStatus.Normal
+    
     var body: some View {
-        HStack{
+        HStack{ // HStack to divide the screen in half, a traditional settings screen design on tvOS
+            
+            // Left side, giant image filler
             ZStack {
                 Image(systemName: "slider.horizontal.below.rectangle").resizable().frame(width:400, height:400).scaledToFill()
             }.frame(width:1920/2, height:400)
-    
-        Form{
-//            TextField("Name", text: $name)
-//            SecureField("Password", text: $password)
             
-            Toggle(isOn: $toggleValue) {
-                Text("Toggle")
+            // Right side, a form of many UI Elements
+            Form{
+                // Status
+                HStack{
+                    Text("Status")
+                    Spacer()
+                    Status(instatus:$status)
+                }
+                
+                // Toggle
+                Toggle(isOn: $toggleValue) {
+                    Text("Toggle")
+                }
+                
+                // Two kinds of progress
+                ProgressView(prog)//.padding(.bottom, 30)
+                ProgressView()
+                
+                // Link
+                Link("External Link", destination: URL(string: "https://www.youtube.com/watch?v=iaJNk8gDxwU")!)
+                
+                // Button and Alert
+                Button(LocalizedStringKey("Show Alert")) {
+                    isShowingAlert = true
+                }
+            }
+            .onAppear(perform: {prog.becomeCurrent(withPendingUnitCount: 1) ; prog.resignCurrent()})
+            .alert("Sample Alert", isPresented: $isShowingAlert) {
+                Button("Continue", role: .cancel) {}
+            } message: {
+                Text("Hello")
             }
             
-            ProgressView(prog)//.padding(.bottom, 30)
-            ProgressView()
-            
-            Link("External Link", destination: URL(string: "https://www.wikipedia.org")!)
-            
-            Button(LocalizedStringKey("Show Alert")) {
-                isShowingAlert = true
-            }
-
-        }
-        .background(Color.astroUIBackground) // set the background color for both sections
-        .navigationBarTitle("Form")
-        .onAppear(perform: {prog.becomeCurrent(withPendingUnitCount: 1) ; prog.resignCurrent()})
-        .alert("Sample Alert", isPresented: $isShowingAlert) {
-            Button("Continue", role: .cancel) {}
-        } message: {
-            Text("Hello")
-        }
-      
-        }  .tabItem {
+        }.navigationBarTitle("Form")
+        .background(Color.astroUIBackground) // Set the background color for the whole page
+        .tabItem {
             Image(systemName: "slider.horizontal.below.rectangle")
             Text("UI Elements")
+        }
+        .onChange(of: toggleValue) { newValue in // Link the change of the toggle control to the status control
+            if (toggleValue){
+                status = .Normal
+            }
+            else {
+                status = .Off
+            }
         }
     }
 }
