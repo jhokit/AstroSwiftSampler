e8c0926dcaf937bb9f89dc7068f75e2b09d6cb35
Jeff Hokit <jhokit@rocketcom.com>
2022-06-24 13:32:38 -0700

    Update tvOS and macOS color views to show variants in sheets

--- tvOS/Color/ColorGrid.swift
+++ tvOS/Color/ColorGrid.swift
@@ -12,38 +12,79 @@ struct ColorGrid: View {
     
     var body: some View {
         
-        let columns:[GridItem] = Array(repeating: .init(.flexible()), count: 3)
-        LazyVGrid(columns:columns, spacing: 18){
-            Section("Semantic"){
-                if let colorVariants = AstroColorSamples.astroSemantic.colorVariants
-                {
-                    ForEach(colorVariants, id: \.id) { colorSample in
-                        ColorTile(sample: colorSample)
-                    }
-                }
-            }
-            
-            Section("Core"){
-                if let colorVariants = AstroColorSamples.astroCore.colorVariants
-                {
-                    ForEach(colorVariants, id: \.id) { colorSample in
-                        Button(action: {
-                            sheetColorSample = colorSample}) {
-                            ColorTile(sample: colorSample)
+        ScrollView{
+            let columns:[GridItem] = Array(repeating: .init(.flexible()), count: 3)
+            LazyVGrid(columns:columns, spacing: 18){
+                Section("Semantic"){
+                    if let colorVariants = AstroColorSamples.astroSemantic.colorVariants
+                    {
+                        ForEach(colorVariants, id: \.id) { colorSample in
+                            if #available(tvOS 16, *) {
+                                ColorTile(sample: colorSample)
+                            } else { // work around a scrolling bug in tvOS 15 by making every tile focusable
+                                Button(action: {}) {
+                                        ColorTile(sample: colorSample)
+                                    }
+                                    .buttonStyle(PlainTileStyle())
+                            }
                         }
-                        .buttonStyle(PlainTileStyle())
+                    }
+                }
+                
+                Section("Status"){
+                    if let colorVariants = AstroColorSamples.astroStatus.colorVariants
+                    {
+                        ForEach(colorVariants, id: \.id) { colorSample in
+                            if #available(tvOS 16, *) {
+                                ColorTile(sample: colorSample)
+                            } else { // work around a scrolling bug in tvOS 15 by making every tile focusable
+                                Button(action: {}) {
+                                        ColorTile(sample: colorSample)
+                                    }
+                                    .buttonStyle(PlainTileStyle())
+                            }
+                        }
+                    }
+                }
+                
+                Section("Classification"){
+                    if let colorVariants = AstroColorSamples.astroClassification.colorVariants
+                    {
+                        ForEach(colorVariants, id: \.id) { colorSample in
+                            if #available(tvOS 16, *) {
+                                ColorTile(sample: colorSample)
+                            } else { // work around a scrolling bug in tvOS 15 by making every tile focusable
+                                Button(action: {}) {
+                                        ColorTile(sample: colorSample)
+                                    }
+                                    .buttonStyle(PlainTileStyle())
+                            }
+                        }
+                    }
+                }
+                
+                Section("Core"){
+                    if let colorVariants = AstroColorSamples.astroCore.colorVariants
+                    {
+                        ForEach(colorVariants, id: \.id) { colorSample in
+                            Button(action: {
+                                sheetColorSample = colorSample}) {
+                                    ColorTile(sample: colorSample)
+                                }
+                                .buttonStyle(PlainTileStyle())
+                        }
                     }
                 }
             }
+            .padding()
+            .background(Color.astroUIBackground)
+            .fullScreenCover(item: $sheetColorSample, content: {theSample in ColorVariantGrid(item: theSample)})
         }
-        .padding()
-        .background(Color.astroUIBackground)
-        .fullScreenCover(item: $sheetColorSample, content: {theSample in ColorVariantGrid(item: theSample)})
     }
 }
 
 
-struct ColorVariantGrid: View {
+private struct ColorVariantGrid: View {
 
     let item: ColorSample
     var body: some View {
@@ -63,7 +104,6 @@ struct ColorVariantGrid: View {
         .frame(maxWidth: .infinity, maxHeight: .infinity) // fill the screen, as simply using fullScreenCover does not
         .padding()
         .background(Color.astroUIBackground)
-        
     }
 }
 
