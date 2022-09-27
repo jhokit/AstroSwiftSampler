bab57037af16f33db5cecbf5e6c1b996200da363
Jeff Hokit <jhokit@rocketcom.com>
2022-09-27 15:00:45 -0700

    fix: add correct default view behavior

--- macOS/ContentView.swift
+++ macOS/ContentView.swift
@@ -8,41 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-  //  @State private var selection: String = "components"
-    let category = ["components","colors","symbols"]
+    @State private var selection: String = "components"
+    let categories = ["components","colors","symbols"] // a way to identify our three view types
     
     var body: some View {
         
         NavigationSplitView{
-            List(){
-                NavigationLink(value: category[0] ) {
+            List(selection:$selection){
+                NavigationLink(value: categories[0] ) {
                     Label("Components", systemImage: "switch.2")
                 }
-                NavigationLink(value: category[1] ) {
+                NavigationLink(value: categories[1] ) {
                     Label("Colors", systemImage: "paintpalette")
                 }
-                NavigationLink(value: category[2] ) {
+                NavigationLink(value: categories[2] ) {
                     Label("Symbols", systemImage: "star")
                 }
-                
             }
-            .listStyle(.sidebar)
             .frame(minWidth:100)
-            .navigationDestination(for: String.self) { theCategory in
-                switch theCategory{
-                case category[0]:
+        } detail: {
+                switch selection{
+                case categories[0]:
                     Components()
-                case category[1]:
+                case categories[1]:
                     ColorGrid()
-                case category[2]:
+                case categories[2]:
                     SymbolGrid()
                 default:
                     EmptyView()
                 }
             }
-        } detail: {
-            // no default view here
-        }
         .frame(minWidth:500, idealWidth: 800)
     }
 }
