38ed37e996071a4a43a61906dd66915d9a45d70f
Jeff Hokit <jhokit@rocketcom.com>
2022-12-07 11:30:17 -0800

    fix: remove experimental styling on bar chart

--- Shared/Charts/BarChart.swift
+++ Shared/Charts/BarChart.swift
@@ -17,19 +17,21 @@ struct BarChart: View {
                     y: .value("Value",item.value)
                 )
                 .foregroundStyle(by: .value("Name",series.name))
-                // example of how to add a divider line at the top of a bar
-                .annotation(position: .top, alignment: .leading, spacing: -1) { context in // starting an one pixel below the bar
-                    if context.targetSize.height > 1 // only if the bar is more than 1 pt high
-                    {
-                        if series.name == "Morning"{    // only for the first series in the stack. How to generalize this for Astro?
-                            Rectangle() // A one pt black line
-                                .fill(Color.black)
-                                .frame(width: context.targetSize.width, height: 1)
-                        }
-                    }
-                }
-// some test code to add a text annoatation within the bar, truncating to nothing if it does not work reliably,
-// because truncation always leaves a ..., which still has a small size
+// some test code to add a a divider line at the top of a bar
+// attempting to get closer to the astro design of a frame around each bar
+//                .annotation(position: .top, alignment: .leading, spacing: -1) { context in // starting an one pixel below the bar
+//                    if context.targetSize.height > 1 // only if the bar is more than 1 pt high
+//                    {
+//                        if series.name == "Morning"{    // only for the first series in the stack. How to generalize this for Astro?
+//                            Rectangle() // A one pt black line
+//                                .fill(Color.black)
+//                                .frame(width: context.targetSize.width, height: 1)
+//                        }
+//                    }
+//                }
+// some test code to add a text annoatation within the bar, truncating as necessary
+// it does not work reliably because truncation always leaves a ..., which still has a small size
+// this causes a very short bar to gow below the x axis, changing the chart scale
 //                .annotation(position: .overlay, alignment: .top, spacing: 0) { context in
 //                    Text(String(item.value))
 //                        .foregroundColor(.black)
