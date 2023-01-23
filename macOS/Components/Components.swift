ef1cf932be9bfa090c55cb09938d70134b498319
Jeff Hokit <jhokit@rocketcom.com>
2023-01-23 11:06:11 -0800

    fix: redesign and reorganize macOS components view

--- macOS/Components/Components.swift
+++ macOS/Components/Components.swift
@@ -10,67 +10,13 @@ import AstroSwiftFoundation
 import AstroSwiftUtilities
 
 struct Components: View {
-    @State var toggleValue: Bool = true
-    @State private var progressValue: Double = 3
-    let progressTotal:Double = 5
-    @State private var isShowingAlert = false
     
     var body: some View {
         ScrollView{
             VStack {
-                Text("Astro Components").font(.headline).padding(.top,10)
-                Form {
-                    // Astro Components
-                    VStack(alignment: .leading,spacing:15){
-                        StatusSymbols()
-                        
-                        StatusTags()
-
-                        Tags()
-
-                        Clocks()
-
-                        Timers()
-
-                        ClassificationBanners()
-                        
-                        ClassificationMarkers()
-                    }
-                }
-                .padding()
+                AstroComponents()
                 Divider()
-               
-                // System Components
-                Text("System Components").font(.headline).padding(.top,2)
-
-                Form(){
-
-                    // Toggle
-                    Toggle(isOn: $toggleValue) {
-                        Text("Toggle")
-                    }
-                    
-                    // Two kinds of progress
-                    ProgressView("Progress", value: progressValue, total: progressTotal)
-                    ProgressView().padding(.bottom, 8)
-                    
-                    // Link
-                    Link("External Link", destination: URL(string: "https://www.astrouxds.com")!)
-                    
-                    
-                    // Button and Alert
-                    Button(LocalizedStringKey("Show Alert")) {
-                        isShowingAlert = true
-                    }
-                    
-                }.padding()
-                    .alert("Sample Alert", isPresented: $isShowingAlert) {
-                        Button("Continue", role: .cancel) {}
-                    } message: {
-                        Text("Hello")
-                    }
-                
-                Divider()
+                SystemComponents()
                 HStack{
                     Text(versionString()).padding()
                     Spacer()
@@ -80,13 +26,127 @@ struct Components: View {
         }
         .background(Color.astroUIBackground) // Set the background color for the whole page
         .navigationTitle("Components")
+        
+    }
+}
+
+struct AstroComponents: View {
+    var body: some View {
+        Text("Astro Components").font(.headline).padding(.top,10)
+        Form {
+            // Astro Components
+            VStack(alignment: .leading,spacing:15){
+                StatusSymbols()
+                
+                StatusTags()
+
+                Tags()
+
+                Clocks()
+
+                Timers()
+
+                ClassificationBanners()
+                
+                ClassificationMarkers()
+            }
+        }
+        .padding()
+    }
+}
+
+struct SystemComponents: View {
+    @State private var isShowingAlert = false
+
+    var body: some View {
+        // System Components
+        Text("System Components").font(.headline).padding(.top,2)
+
+        Form(){
+            VStack(alignment: .leading,spacing:15){
+                
+                ToggleSample()
+                
+                Progress()
+                
+                ExternalLink()
+                
+                ButtonAndAlert(isShowingAlert: $isShowingAlert)
+            }
+
+        }.padding()
+            .alert("Sample Alert", isPresented: $isShowingAlert) {
+                Button("Continue", role: .cancel) {}
+            } message: {
+                Text("Hello")
+            }
+        
+        Divider()
 
     }
 }
 
+
+struct ToggleSample: View {
+    @State var toggleValue: Bool = true
+
+    var body: some View {
+        HStack{
+            Text("Toggle").font(.subheadline)
+            Spacer()
+        }.padding(.bottom,4)
+        Toggle(isOn: $toggleValue) {
+            Text("Value")
+        }
+        Divider()
+    }
+}
+
+struct Progress: View {
+    @State private var progressValue: Double = 3
+    let progressTotal:Double = 5
+
+    var body: some View{
+        // Two kinds of progress
+        HStack{
+            Text("Progress").font(.subheadline)
+            Spacer()
+        }.padding(.bottom,4)
+
+        ProgressView("Determinate", value: progressValue, total: progressTotal)
+        ProgressView().padding(.bottom, 8)
+        Divider()
+    }
+}
+
+            
+struct ExternalLink: View {
+    var body: some View {
+        HStack{
+            Text("External Link").font(.subheadline)
+            Spacer()
+        }.padding(.bottom,4)
+        Link("Astro Web Site", destination: URL(string: "https://www.astrouxds.com")!)
+        Divider()
+    }
+}
+
+struct ButtonAndAlert: View {
+    @Binding var isShowingAlert:Bool
+    var body: some View {
+        HStack{
+            Text("Button and Alert").font(.subheadline)
+            Spacer()
+        }.padding(.bottom,4)
+        Button(LocalizedStringKey("Show Alert")) {
+            isShowingAlert = true
+        }
+    }
+}
+    
 struct StatusSymbols: View {
     var body: some View {
-        Text("Status")
+        Text("Status").font(.subheadline)
         HStack{
             Status(AstroStatus.off)
             Status(AstroStatus.standby)
@@ -104,7 +164,7 @@ struct Clocks: View {
     var body: some View {
         VStack(alignment: .leading, spacing:6){
             HStack{
-                Text("Clock")
+                Text("Clock").font(.subheadline)
                 Spacer()
             }.padding(.bottom,4)
             // standard Astro Clock, equivalent to AstroClock(verbatimFormatter: AstroClock.astroDayTime)
@@ -155,7 +215,7 @@ struct Timers: View {
     var body: some View {
         VStack(alignment: .leading){
             HStack{
-                Text("Interval Timer")
+                Text("Interval Timer").font(.subheadline)
                 Spacer()
             }.padding(.bottom,4)
             VStack(alignment: .trailing, spacing:6) {
@@ -172,7 +232,7 @@ struct Timers: View {
 struct StatusTags: View {
     var body: some View {
         VStack(alignment: .leading){
-            Text("Status Tag")
+            Text("Status Tag").font(.subheadline)
             HStack(){
                 Tag(text:AstroStatus.off.description,status: .off)
                 Tag(text:AstroStatus.standby.description,status: .standby)
@@ -191,7 +251,7 @@ struct StatusTags: View {
 struct Tags: View {
     var body: some View {
         VStack(alignment: .leading){
-            Text("Tag")
+            Text("Tag").font(.subheadline)
             HStack{
                 Tag(text:"Hello")
                 Spacer()
@@ -204,7 +264,7 @@ struct Tags: View {
 struct ClassificationBanners: View {
     var body: some View {
         HStack{
-            Text("Classification Banner")
+            Text("Classification Banner").font(.subheadline)
             Spacer()
         }
         ClassificationBanner(.unclassified)
@@ -222,7 +282,7 @@ struct ClassificationMarkers: View {
     var body: some View {
         VStack{
             HStack{
-                Text("Classification Marker")
+                Text("Classification Marker").font(.subheadline)
                 Spacer()
             }
             HStack(){
@@ -242,6 +302,6 @@ struct ClassificationMarkers: View {
 //// showing the entire view causes simulator to crash
 struct Components_Previews: PreviewProvider {
     static var previews: some View {
-        Components()
+        SystemComponents()
     }
 }
